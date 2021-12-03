128edc65721d4bdcb716fd3db82199f089181c90
jiaaren <jiaren.kjr@gmail.com>
2021-12-03 16:11:56 +0800

    modified getInt to call getFloat

--- 02/submit/ex01/Fixed.cpp
+++ 02/submit/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 00:59:28 by jkhong            #+#    #+#             */
-/*   Updated: 2021/12/03 02:09:42 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/12/03 12:56:24 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,78 +16,78 @@
 
 Fixed::Fixed(void) : _fpval(0)
 {
-	std::cout << "Default constructor called" << std::endl;
-	return;
+    std::cout << "Default constructor called" << std::endl;
+    return;
 }
 
 Fixed::Fixed(Fixed const &n)
 {
-	std::cout << "Copy constructor called" << std::endl;
-	*this = n;
-	return;
+    std::cout << "Copy constructor called" << std::endl;
+    *this = n;
+    return;
 }
 
 Fixed::Fixed(int const n)
 {
-	std::cout << "Int constructor called" << std::endl;
-	// essentially n ^ 8
-	this->_fpval = n << _frac_bits;
-	return;
+    std::cout << "Int constructor called" << std::endl;
+    // essentially n ^ 8
+    this->_fpval = n << _frac_bits;
+    return;
 }
 
 Fixed::Fixed(float const f)
 {
-	float tmp = f;
-	for (int i = 0; i < 8; i++)
-		tmp *= 2;
-	this->_fpval = (int)(roundf(tmp));
-	std::cout << "Float constructor called" << std::endl;
-	return;
+    float tmp = f;
+    for (int i = 0; i < 8; i++)
+        tmp *= 2;
+    this->_fpval = (int)(roundf(tmp));
+    std::cout << "Float constructor called" << std::endl;
+    return;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
-	return;
+    std::cout << "Destructor called" << std::endl;
+    return;
 }
 
 Fixed &Fixed::operator=(Fixed const &rhs)
 {
-	std::cout << "Assignation operator called" << std::endl;
-	this->_fpval = rhs.getRawBits();
-	return (*this);
+    std::cout << "Assignation operator called" << std::endl;
+    this->_fpval = rhs.getRawBits();
+    return (*this);
 }
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
-	return (this->_fpval);
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_fpval);
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
-	this->_fpval = raw;
-	return;
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fpval = raw;
+    return;
 }
 
 float Fixed::toFloat(void) const
 {
-	float tmp = (float)(this->_fpval);
-	for (int i = 0; i < 8; i++)
-		tmp /= 2;
-	return (tmp);
+    float tmp = (float)(this->_fpval);
+    for (int i = 0; i < 8; i++)
+        tmp /= 2;
+    return (tmp);
 }
 
 int Fixed::toInt(void) const
 {
-	return (this->_fpval >> _frac_bits);
+    return (this->toFloat());
 }
 
 int const Fixed::_frac_bits = 8;
 
 std::ostream &operator<<(std::ostream &o, Fixed const &rhs)
 {
-	o << rhs.toFloat();
-	return (o);
+    o << rhs.toFloat();
+    return (o);
 }
--- 02/submit/notes.md
+++ 02/submit/notes.md
@@ -1,3 +1,7 @@
 Err
 - copy constructor may not have a parameter of type
 - https://stackoverflow.com/questions/9066052/copy-constructor-parameters
+
+
+Ex01
+- roundf - floor makes negative numbers round up
