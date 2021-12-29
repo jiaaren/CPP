65f10de2445eb4ad09de1a4194f4e9c3c183734b
jiaaren <jiaren.kjr@gmail.com>
2021-12-30 00:30:42 +0800

    test const iterator and const reverse iterator

--- 08/submit/ex02/main.cpp
+++ 08/submit/ex02/main.cpp
@@ -2,39 +2,70 @@
 #include <list>
 #include "mutantstack.hpp"
 
+void print(std::string str)
+{
+	std::cout << str << "\n";
+}
+
 int main(void)
 {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
 
-    MutantStack<int> another(mstack);
-    std::cout << another.top() << std::endl;
+	MutantStack<int> another(mstack);
+	std::cout << "another " << another.top() << std::endl;
+	another.pop();
+	std::cout << another.top() << std::endl;
 
-    std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl;
 
-    mstack.pop();
+	mstack.pop();
 
-    std::cout << mstack.size() << std::endl;
+	std::cout << mstack.size() << std::endl;
 
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
 
-    MutantStack<int>::iterator const it;
-    it = mstack.begin();
-    (void)it;
-    // MutantStack<int>::iterator ite = mstack.end();
+	print("\n<---- Iterating using standard iterator (and modifying value subsequently after printing)---->");
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		(*it)++;
+		++it;
+	}
 
-    // ++it;
-    // --it;
-    // while (it != ite)
-    // {
-    //     std::cout << *it << std::endl;
-    //     ++it;
-    // }
-    // std::stack<int> s(mstack);
-    return (0);
+	print("\n<---- Iterating using const iterator ---->");
+	MutantStack<int>::const_iterator cit = mstack.cbegin();
+	MutantStack<int>::const_iterator cite = mstack.cend();
+	while (cit != cite)
+	{
+		std::cout << *cit << std::endl;
+		++cit;
+	}
+
+	print("\n<---- Iterating using standard reverse iterator (and modifying value subsequently after printing)---->");
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		(*rit)++;
+		++rit;
+	}
+
+	print("\n<---- Iterating using const reverse iterator ---->");
+	MutantStack<int>::const_reverse_iterator crit = mstack.crbegin();
+	MutantStack<int>::const_reverse_iterator crite = mstack.crend();
+	while (crit != crite)
+	{
+		std::cout << *crit << std::endl;
+		++crit;
+	}
+	return (0);
 }
--- 08/submit/ex02/mutantstack.hpp
+++ 08/submit/ex02/mutantstack.hpp
@@ -10,129 +10,79 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack(void);
-	MutantStack(MutantStack const &ms);
-	virtual ~MutantStack(void){};
-	// create iterator class
-	class iterator : public std::list<T>::const_iterator
-	{
-		// pre increment and decrement - copy from std::list iterator
-		// != operator - copy from std::list iterator
-	};
-	// iterator method for begin and end
-	iterator const &begin(void) const;
-	iterator const &end(void) const;
-	void push(T n);
-	void pop(void);
-
-private:
-	std::list<T> _l;
-	void _copyToList(void);
+    // MutantStack(void);
+    // MutantStack(MutantStack const &ms);
+    // virtual ~MutantStack(void){};
+    // create iterator class
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+    MutantStack &operator=(MutantStack &ms);
+    // iterator method for begin and end, cannot have const function to return nothing...
+    iterator begin(void);
+    iterator end(void);
+    const_iterator cbegin(void);
+    const_iterator cend(void);
+    reverse_iterator rbegin(void);
+    reverse_iterator rend(void);
+    const_reverse_iterator crbegin(void);
+    const_reverse_iterator crend(void);
 };
 
-// template <typename T>
-// class MutantStack : public std::stack<T>
-// {
-// private:
-// 	std::list<T> _l;
-// 	void _copyToList(void);
-
-// public:
-// 	MutantStack(void);
-// 	MutantStack(MutantStack const &ms);
-// 	virtual ~MutantStack(void){};
-// 	// create iterator class
-// 	typedef std::list<T> base_list;
-// 	typedef typename base_list::iterator iterator;
-// 	typedef typename base_list::const_iterator const_iterator;
-// 	// iterator method for begin and end
-// 	using _l::begin;
-// 	using _l::end;
-// 	void push(T n);
-// 	void pop(void);
-// };
-
-/*
-- this seems unnecessary now
-mutantstack.hpp:36:13: error: there are no arguments to ‘size’ that depend on a template parameter, so a declaration of ‘size’ must be available [-fpermissive]
-   36 |     int s = size();
-	  |             ^~~~
-https://stackoverflow.com/questions/9941987/there-are-no-arguments-that-depend-on-a-template-parameter
-*/
+// std::stack<T> refers to the base class
 template <typename T>
-void MutantStack<T>::_copyToList(void)
+typename MutantStack<T>::iterator MutantStack<T>::begin(void)
 {
-	std::stack<T> dup(static_cast<std::stack<T> >(*this));
-	// execute this from base class
-	int s = std::stack<T>::size();
-	for (int i = 0; i < s; i++)
-	{
-		_l.push_back(dup.top());
-		dup.pop();
-	}
-	return;
+    return (std::stack<T>::c.begin());
 }
 
-// base class constructor void no need to explicitly call
 template <typename T>
-MutantStack<T>::MutantStack(void) : std::stack<T>()
+typename MutantStack<T>::iterator MutantStack<T>::end(void)
 {
-	std::cout << "executed" << std::endl;
+    return (std::stack<T>::c.end());
 }
 
-// need copy constructor to run base class std::stack<T>'s copy constructor
 template <typename T>
-MutantStack<T>::MutantStack(MutantStack const &ms) : std::stack<T>(ms)
+typename MutantStack<T>::const_iterator MutantStack<T>::cend(void)
 {
-	(void)ms;
-	std::cout << "executed again" << std::endl;
-	_copyToList();
+    return (std::stack<T>::c.cend());
 }
 
-template<typename T>
-void MutantStack<T>::push(T n)
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cbegin(void)
 {
-	// need to run base class functions again
-	std::stack<T>::push(n);
-	_l.push_front(n);
-	return;
+    return (std::stack<T>::c.cbegin());
 }
 
-template<typename T>
-void MutantStack<T>::pop(void)
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend(void)
 {
-	// need to run base class functions again
-	std::stack<T>::pop();
-	_l.pop_front();
-	return;
+    return (std::stack<T>::c.rend());
 }
 
-/*
-mutantstack.hpp:90:1: error: need ‘typename’ before ‘MutantStack<T>::iterator’ because ‘MutantStack<T>’ is a dependent scope
-   90 | MutantStack<T>::iterator &MutantStack<T>::begin(void) const
-	  | ^~~~~~~~~~~~~~
-	  | typename 
-
-./mutantstack.hpp:119:9: error: no viable conversion from returned value of type 'std::__1::list<int, std::__1::allocator<int> >::const_iterator' (aka '__list_const_iterator<int, void *>') to function return type
-      'MutantStack<int>::iterator'
-        return (_l.begin());
-
-./mutantstack.hpp:17:8: note: candidate constructor (the implicit copy constructor) not viable: cannot bind base class object of type 'std::__1::list<int, std::__1::allocator<int> >::const_iterator'
-      (aka '__list_const_iterator<int, void *>') to derived class reference 'const MutantStack<int>::iterator &' for 1st argument
-        class iterator : public std::list<T, std::allocator<T> >::const_iterator
-              ^
-- added reference (&) to silence error
-*/
-template<typename T>
-typename MutantStack<T>::iterator const &MutantStack<T>::begin(void) const
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin(void)
 {
-	return (_l.begin());
+    return (std::stack<T>::c.rbegin());
 }
 
-template<typename T>
-typename MutantStack<T>::iterator const &MutantStack<T>::end(void) const
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crbegin(void)
 {
-	return (_l.end());
+    return (std::stack<T>::c.crbegin());
+}
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crend(void)
+{
+    return (std::stack<T>::c.crend());
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(MutantStack<T> &ms)
+{
+    std::stack<T>::operator=(ms);
 }
 
 #endif
