7e255ce224cd1568a7dafe7570bc4c7a56c21c25
Jia ren Khong <jkhong@u90z04s03.42kl.edu.my>
2021-12-30 09:37:24 +0800

    slight changes made to main.cpp ex01 CPP08

--- 08/submit/ex01/main.cpp
+++ 08/submit/ex01/main.cpp
@@ -13,7 +13,7 @@ int main(void)
 	{
 		std::cout << "<--- Declare Span --->\n";
 		Span sp = Span(5);
-		sp.addNumber(1);
+		sp.addNumber(5);
 		sp.addNumber(3);
 		sp.addNumber(17);
 		sp.addNumber(9);
